674d9925c96413cac6a9b6006131665b0a31cbf4
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-05-11 19:07:09 +1200

    add `Config` type

---
+++ server/src/types/Config.ts
@@ -0,0 +1,49 @@
+import { ValuesOf, JSONValue } from './Utility';
+
+/**
+ * Shape of exported config that will be used throughout the server.
+ *
+ * For more info see the schema (`docs/config-schema.json`).
+ *
+ * For default values, see `src/constants/defaultConfig.ts`.
+ */
+export interface Config {
+    port: number;
+
+    clientUrls: Set<string>;
+
+    numProxies: number;
+
+    maxRequestsPerMinute: number;
+
+    rateLimitBypassTokens: Set<string>;
+
+    /** Required value, process will exit if omitted! */
+    mongoURI: string;
+
+    mongoDbName: string;
+
+    // the following values should not be defined in `config.json` as they are automatically generated
+    // if provided, they will be overwritten
+
+    /** Commit hash of current version. */
+    commit: string;
+
+    startedAt: string;
+}
+
+/**
+ * These properties must be transformed by our config loading function, as their type in the {@link Config} interface
+ * cannot be represented in JSON ({@link Set Sets}, {@link RegExp RegExps}, etc...).
+ */
+// there's probably a better way of extracting these keys out, however I haven't found it :P
+type KeysThatRequireTransforming = ValuesOf<{ [k in keyof Config]: Config[k] extends JSONValue ? never : k }>;
+
+/**
+ * This interface defines the initial JSON-representable type of
+ * {@link KeysThatRequireTransforming properties that need transforming}.
+ */
+export interface ImportedConfig extends Partial<Omit<Config, KeysThatRequireTransforming>> {
+    clientUrls?: string[];
+    rateLimitBypassTokens?: string[];
+}
