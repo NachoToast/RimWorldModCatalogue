b6d9f62cb8de1371270f3a359640bbd711e03f37
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-05-12 20:43:44 +1200

    add `ModService`

---
+++ server/src/classes/ModService.ts
@@ -0,0 +1,25 @@
+import { AnyBulkWriteOperation } from 'mongodb';
+import { Mod, ModModel } from '../types/Mod';
+
+/** Handles all interactions with the mods database. */
+export class ModService {
+    private readonly _model: ModModel;
+
+    public constructor(modModel: ModModel) {
+        this._model = modModel;
+    }
+
+    public async upsert(mods: Mod[]): Promise<void> {
+        if (mods.length === 0) return;
+
+        const bulkUpdateOperations = mods.map<AnyBulkWriteOperation<Mod>>((mod) => ({
+            updateOne: {
+                filter: { _id: mod._id },
+                update: { $set: mod },
+                upsert: true,
+            },
+        }));
+
+        await this._model.bulkWrite(bulkUpdateOperations);
+    }
+}
