4f66a1d3d72dbc5c329a6df0f1e7d592180ca056
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-05-12 20:43:32 +1200

    add `MetadataService`

---
+++ server/src/classes/MetadataService.ts
@@ -0,0 +1,19 @@
+import { DatabaseMetadata, DatabaseMetadataModel } from '../types/DatabaseMetadata';
+
+/** Handles retreiving and updating database metadata. */
+export class MetadataService {
+    private _model: DatabaseMetadataModel;
+
+    public constructor(databaseMetadataModel: DatabaseMetadataModel) {
+        this._model = databaseMetadataModel;
+    }
+
+    public async getDatabaseMetadata(): Promise<DatabaseMetadata | null> {
+        const metadata = await this._model.findOne({});
+        return metadata;
+    }
+
+    public async updateDatabaseMetadata(metadata: DatabaseMetadata): Promise<void> {
+        await this._model.updateOne({}, { $set: metadata }, { upsert: true });
+    }
+}
--- server/src/loaders/loadMongo.ts
+++ server/src/loaders/loadMongo.ts
@@ -1,9 +1,12 @@
 import { MongoClient } from 'mongodb';
 import { Colours } from '../types/Colours';
 import { Config } from '../types/Config';
+import { DatabaseMetadataModel } from '../types/DatabaseMetadata';
 import { ModModel } from '../types/Mod';
 
-export async function loadMongo(config: Config): Promise<[ModModel, MongoClient]> {
+export async function loadMongo(
+    config: Config,
+): Promise<{ modModel: ModModel; databaseMetadataModel: DatabaseMetadataModel; mongoClient: MongoClient }> {
     if (config.mongoDbName.length > 38) {
         console.log(
             `${Colours.FgRed}Mongo DB name cannot be more than 38 characters long (configured is ${config.mongoDbName.length})${Colours.Reset}`,
@@ -17,7 +20,9 @@ export async function loadMongo(config: Config): Promise<[ModModel, MongoClient]
 
     const modModel: ModModel = db.collection('mods');
 
+    const databaseMetadataModel: DatabaseMetadataModel = db.collection('meta');
+
     await modModel.createIndex({ description: 'text' });
 
-    return [modModel, mongoClient];
+    return { modModel, databaseMetadataModel, mongoClient };
 }
