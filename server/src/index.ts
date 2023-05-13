0cdd189248c3610f9b8f7b76d92a221d045bab3a
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-05-13 19:28:01 +1200

    update app starting functions

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,9 @@
 import { schedule } from 'node-cron';
 import { loadConfig } from './loaders/loadConfig';
-import { loadExpress } from './loaders/loadExpress';
+import { loadExpress, startApp } from './loaders/loadExpress';
 import { loadMongo } from './loaders/loadMongo';
-import { applyRoutes } from './routes';
 import { initializeModService } from './services/ModService';
 import { getLastUpdate, initializeUpdateService, performUpdate } from './services/UpdateService';
-import { Colours } from './types/Colours';
-import { Config } from './types/Config';
 
 async function main() {
     const config = loadConfig();
@@ -15,35 +12,15 @@ async function main() {
     initializeUpdateService(db);
     await initializeModService(db);
 
-    startAPI(config);
+    const app = loadExpress(config);
+
+    await startApp(app, config.port);
 
     schedule('0 */6 * * *', performUpdate); // every 6 hours
 
     if ((await getLastUpdate()) === null) {
         performUpdate();
     }
-}
-
-function startAPI(config: Config): void {
-    const app = loadExpress(config);
-
-    applyRoutes(app, config);
-
-    const server = app.listen(config.port, () => {
-        const _addr = server.address();
-
-        let address, port;
-
-        if (typeof _addr !== 'string' && _addr !== null) {
-            address = _addr.address.replace('::', 'localhost');
-            port = _addr.port;
-        } else {
-            address = 'unknown';
-            port = config.port;
-        }
-
-        console.log(`Listening on ${Colours.FgCyan}http://${address}:${port}${Colours.Reset} (${app.get('env')})`);
-    });
 }
 
 main();
--- server/src/loaders/loadExpress.ts
+++ server/src/loaders/loadExpress.ts
@@ -1,6 +1,13 @@
 /* eslint-disable import/no-named-as-default-member */
 import express, { Express } from 'express';
 import { serve, setup } from 'swagger-ui-express';
+import { corsMiddleware } from '../middleware/corsMiddleware';
+import { rateLimitingMiddleware } from '../middleware/rateLimitingMiddleware';
+import { siteErrorHandler } from '../middleware/siteErrorHandler';
+import { validatorErrorHandler } from '../middleware/validatorErrorHandler';
+import { validatorMiddleware } from '../middleware/validatorMiddleware';
+import { applyRoutes } from '../routes';
+import { Colours } from '../types/Colours';
 import { Config } from '../types/Config';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const apiSpec = require('../../openapi.json');
@@ -16,5 +23,44 @@ export function loadExpress(config: Config): Express {
 
     app.use(express.json());
 
+    {
+        // pre-route middleware (e.g. validation, authentication)
+        app.use(express.json());
+        app.use(corsMiddleware(config));
+        app.use(rateLimitingMiddleware(config));
+        app.use(validatorMiddleware(config));
+        // this error handler is pre-route since validator errors are thrown by the validator middleware, meaning we
+        // can catch them before the route is called
+        app.use(validatorErrorHandler(config));
+    }
+
+    applyRoutes(app, config);
+
+    {
+        // post-route middleware (e.g. error catching).
+        app.use(siteErrorHandler(config));
+    }
+
     return app;
+}
+
+export function startApp(app: Express, port: number): Promise<void> {
+    return new Promise<void>((resolve) => {
+        const server = app.listen(port, () => {
+            const _addr = server.address();
+
+            let address, port;
+
+            if (typeof _addr !== 'string' && _addr !== null) {
+                address = _addr.address.replace('::', 'localhost');
+                port = _addr.port;
+            } else {
+                address = 'unknown';
+            }
+
+            console.log(`Listening on ${Colours.FgCyan}http://${address}:${port}${Colours.Reset} (${app.get('env')})`);
+
+            resolve();
+        });
+    });
 }
