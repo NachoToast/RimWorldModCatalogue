a2d6a34fbb2c6b41e5cb9e4f0cee56b1b2c5ce33
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-05-13 21:01:40 +1200

    add `searchMods` function to `ModService`

--- server/src/services/ModService.ts
+++ server/src/services/ModService.ts
@@ -8,14 +8,19 @@ This services handles all interactions with the mods database, such as:
 - Searching mods.
 */
 
-import { AnyBulkWriteOperation, Collection, Db } from 'mongodb';
+import { AnyBulkWriteOperation, Collection, Condition, Db, Filter, FindOptions, Sort } from 'mongodb';
 import { Mod } from '../types/Mod';
+import { ModDLCs } from '../types/ModDLCs';
+import { ModSearchOptions } from '../types/ModSearchOptions';
+import { ModSortOptions } from '../types/ModSortOptions';
+import { ModTags } from '../types/ModTags';
+import { WithPagination } from '../types/Page';
 
 let model: Collection<Mod> | null = null;
 
 export async function initializeModService(mongoDb: Db): Promise<void> {
-    model = mongoDb.collection('meta');
-    await model.createIndex({ description: 'text' });
+    model = mongoDb.collection('mods');
+    await model.createIndex({ title: 'text' });
 }
 
 function getModel(): Collection<Mod> {
@@ -39,4 +44,65 @@ export async function upsert(mods: Mod[]): Promise<void> {
     }));
 
     await getModel().bulkWrite(bulkUpdateOperations);
+}
+
+export async function searchMods(searchOptions: ModSearchOptions): Promise<WithPagination<Mod>> {
+    const { page, perPage, sortBy, sortDirection, tagsInclude, tagsExclude, dlcsInclude, dlcsExclude, search } =
+        searchOptions;
+
+    const dlcFilter: Condition<ModDLCs> = { $bitsAllClear: dlcsExclude };
+    if (dlcsInclude) dlcFilter.$bitsAnySet = dlcsInclude;
+
+    const tagFilter: Condition<ModTags> = { $bitsAllClear: tagsExclude };
+    if (tagsInclude) tagFilter.$bitsAnySet = tagsInclude;
+
+    const filter: Filter<Mod> = { dlcs: dlcFilter, tags: tagFilter };
+    const options: FindOptions<Mod> = { limit: perPage, skip: page * perPage };
+    const sort: Sort = {};
+
+    if (search !== undefined) {
+        filter.$text = { $search: search };
+        sort.score = { $meta: 'textScore' };
+    }
+
+    switch (sortBy) {
+        case ModSortOptions.Id:
+            sort._id = sortDirection;
+            break;
+        case ModSortOptions.StarRating:
+            sort.ratingStars = sortDirection;
+            sort.statsSubscribers = sortDirection;
+            break;
+        case ModSortOptions.TotalViews:
+            sort.statsVisitors = sortDirection;
+            break;
+        case ModSortOptions.TotalDownloads:
+            sort.statsSubscribers = sortDirection;
+            break;
+        case ModSortOptions.TotalFavourites:
+            sort.statsFavourites = sortDirection;
+            break;
+        case ModSortOptions.FileSize:
+            sort.size = sortDirection;
+            break;
+        case ModSortOptions.DateUploaded:
+            sort.posted = sortDirection;
+            break;
+        case ModSortOptions.LastUpdated:
+            sort.updated = sortDirection;
+            sort.posted = sortDirection;
+            break;
+        case ModSortOptions.CatalogueLastUpdated:
+            sort.catalogueLastUpdated = sortDirection;
+            break;
+    }
+
+    if (sortBy !== ModSortOptions.Id) sort._id = 1;
+
+    const [totalItemCount, items] = await Promise.all([
+        getModel().countDocuments(filter),
+        getModel().find(filter, options).sort(sort).toArray(),
+    ]);
+
+    return { totalItemCount, items };
 }
