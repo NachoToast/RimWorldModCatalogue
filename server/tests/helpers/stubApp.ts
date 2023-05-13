e54251df9f6ad1e1549e70fd9d9458a86ecd960a
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-05-13 19:12:38 +1200

    `stubApp` test helper function

---
+++ server/tests/helpers/stubApp.ts
@@ -0,0 +1,37 @@
+import express, { RequestHandler } from 'express';
+import request from 'supertest';
+import { Config } from '../../src/types/Config';
+import { MiddlewareProvider } from '../../src/types/MiddlewareProvider';
+import { mockedConfig } from '../mocks/mockedConfig';
+
+/** Creates a stub Express app which responds with status code 200 for `GET /` */
+export function stubApp(
+    partialConfig?: Partial<Config>,
+    preRouteMiddlewares?: MiddlewareProvider[],
+    postRouteMiddlewares?: MiddlewareProvider[],
+    route?: RequestHandler,
+): request.SuperTest<request.Test> {
+    const app = express();
+
+    const config = { ...mockedConfig, ...partialConfig } as Config;
+
+    if (preRouteMiddlewares) {
+        for (const middleware of preRouteMiddlewares) {
+            app.use(middleware(config));
+        }
+    }
+
+    if (route) {
+        app.get('/', route);
+    } else {
+        app.get('/', (_req, res) => res.sendStatus(200));
+    }
+
+    if (postRouteMiddlewares) {
+        for (const middleware of postRouteMiddlewares) {
+            app.use(middleware(config));
+        }
+    }
+
+    return request(app);
+}
