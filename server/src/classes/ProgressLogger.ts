259dc4cccd128c371a558b39ee35975ab1d618ab
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-05-15 19:26:45 +1200

    `ProgressLogger` enhancements

--- server/src/classes/ProgressLogger.ts
+++ server/src/classes/ProgressLogger.ts
@@ -1,6 +1,3 @@
-import { Colours } from '../types/Colours';
-import { FetchError } from '../types/FetchError';
-
 /**
  * Handles routine logging of progress of multiple asynchronous tasks to `process.stdout`.
  *
@@ -10,7 +7,6 @@ import { FetchError } from '../types/FetchError';
  * 1. Instantiation
  * 2. {@link log Logging}
  * 3. {@link close Closing}
- * 4. (Optional) {@link logErrors Logging Errors}
  */
 export class ProgressLogger {
     /** Time between log messages, in seconds. */
@@ -18,6 +14,9 @@ export class ProgressLogger {
 
     /** Maximum character width of the terminal. */
     private readonly _maxWidth: number;
+
+    /** Title string to lead log messages with. */
+    private readonly _title: string;
 
     /** Output to log every interval, each item in this array should represent an asynchronous task. */
     private readonly _output: string[];
@@ -28,11 +27,14 @@ export class ProgressLogger {
     /** Internal interval tracker, referenced for cleanup. */
     private _interval: NodeJS.Timer;
 
+    /** Whether to actually write to the console, prevents writes when the output is unchanged. */
+    private _dirty = false;
+
     /**
      * Creates a new {@link ProgressLogger} instance.
      * @param {number} numItems The number of tasks to track.
      */
-    public constructor(numItems: number) {
+    public constructor(title: string, numItems: number) {
         try {
             this._maxWidth = process.stdout.getWindowSize()[0];
         } catch (error) {
@@ -40,68 +42,49 @@ export class ProgressLogger {
             this._maxWidth = -1;
         }
 
-        this._output = new Array<string>(numItems).fill('-');
+        this._title = title;
+        this._output = new Array(numItems).fill('-');
         this._rows = Math.ceil(numItems / this._maxWidth);
-        this._interval = setInterval(() => this.update(true), ProgressLogger._loggingInterval);
+        this._interval = setInterval(() => {
+            if (this._dirty) {
+                this._dirty = false;
+                this.clear();
+                this.update();
+            }
+        }, ProgressLogger._loggingInterval);
 
-        this.update(false);
+        this.update();
     }
 
     /**
      * Updates the status of a tracked asynchronous task.
      * @param {number} index The index of the task to update.
-     * @param {string} value New status value to log.
+     * @param {string} status New status to show this index as.
      */
-    public log(index: number, value: string): void {
-        this._output[index] = value;
+    public log(index: number, status: string): void {
+        this._output[index] = status;
+        this._dirty = true;
     }
 
-    /** Logs all task statuses to `process.stdout`, clearing the previous output (if supported). */
-    private update(replace: boolean): void {
-        if (replace && this._maxWidth !== -1) {
-            for (let i = 0; i < this._rows; i++) {
-                process.stdout.clearLine(0);
-                process.stdout.moveCursor(0, -1);
-            }
+    /** Clears the previous output (if supported). */
+    private clear(): void {
+        if (this._maxWidth === -1) return;
+
+        for (let i = 0; i <= this._rows; i++) {
+            process.stdout.clearLine(0);
+            process.stdout.moveCursor(0, -1);
         }
+    }
+
+    /** Logs all task statuses to `process.stdout`. */
+    private update(): void {
+        console.log(this._title);
         console.log(this._output.join(''));
     }
 
     /** Marks logging as finished. */
     public close(): void {
         clearInterval(this._interval);
-        this.update(true);
-    }
-
-    /**
-     * Logs an array of errors in a nice format.
-     * @param {FetchError[]} errorArray Array of error tuples, where the first item is the key and the second is
-     * the error message.
-     * @param {string} title Title of the error log.
-     */
-    public static logErrors(errorArray: FetchError[], title: string): void {
-        if (errorArray.length === 0) return;
-
-        console.log(`${Colours.FgRed}${errorArray.length} Errored ${title}:${Colours.Reset}`);
-
-        const maxKeyLength = Math.max(...errorArray.map((e) => e.key.length));
-        const maxTotalLength = Math.max(...errorArray.map((e) => e.message.length)) + maxKeyLength + 10;
-
-        let errorsPerLine: number;
-        try {
-            errorsPerLine = Math.floor(process.stdout.getWindowSize()[0] / maxTotalLength);
-        } catch (error) {
-            // some environments don't seem to have a process.stdout, such as pm2
-            errorsPerLine = 1;
-        }
-
-        for (let i = 0, len = errorArray.length; i < len; i += errorsPerLine) {
-            console.log(
-                errorArray
-                    .slice(i, i + errorsPerLine)
-                    .map((e) => `${e.key.padStart(maxKeyLength, ' ')}: ${e.message}`.padEnd(maxTotalLength, ' '))
-                    .join('|'),
-            );
-        }
+        this.clear();
     }
 }
